1a540c2fb44d02b13c9504fa0b8e1add5799288c
dngur6344 <43164924+dngur6344@users.noreply.github.com>
2019-02-24 21:38:04 +0900

    1697번 - dfs로 풀었는데 시간초과

--- 1697.cpp
+++ 1697.cpp
@@ -1,40 +1,25 @@
 #include<iostream>
 #include<queue>
 using namespace std;
-int mini=-1;
-queue<int> q;
-void searching(int a,int b,int cnt){
-    q.push(a);
-    int flag=0;
-    int tmp=1;
-    while(1){
-        for(int i=0;i<tmp;i++){
-            /*if(a-1>=0){
-             q.push(a-1);
-             }*/
-            if(q.front()==b){
-                flag=1;
-                break;
-            }
-            q.push(q.front()-1);
-            q.push(q.front()+1);
-            q.push(2*q.front());
-            q.pop();
-        }
-        if(flag==1)
-            break;
-        cnt++;
-        tmp=1;
-        for(int i=0;i<cnt;i++){
-            tmp=tmp*3;
-        }
-    }
-    mini=cnt;
+int mini = -1;
+void searching(int a, int b, int cnt) {
+	if (mini < cnt||a<0)
+		return;
+	if (a == b) {
+		mini = cnt;
+		return;
+	}
+	searching(a - 1, b, cnt+1);
+	searching(a + 1, b, cnt+1);
+	searching(2 * a, b, cnt+1);
 }
-int main(void){
-    int N,K;
-    cin>>N>>K;
-    searching(N,K,0);
-    cout<<mini<<endl;
-    return 0;
+int main(void) {
+	int N, K;
+	cin >> N >> K;
+	mini = K - N;
+	if (mini < 0)
+		mini = -mini;
+	searching(N, K, 0);
+	cout << mini << endl;
+	return 0;
 }
