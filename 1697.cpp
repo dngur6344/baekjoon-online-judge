0bff6b06b985bc23c1e9d2a27d0b2ddc442bb95b
dngur6344 <43164924+dngur6344@users.noreply.github.com>
2019-02-27 01:17:34 +0900

    1697번-DFS, 백트래킹인데 초기 mini값을 다르게 설정해봄 그래도 시간초과...

--- 1697.cpp
+++ 1697.cpp
@@ -3,26 +3,41 @@
 using namespace std;
 int mini = -1;
 void searching(int a, int b, int cnt) {
-	if (mini < cnt||a<0)
+	if (mini < cnt || a<0)
 		return;
 	if (a == b) {
 		mini = cnt;
 		return;
 	}
 	if (b % 2 == 0) {
-		searching(a, b/2, cnt + 1);
-		searching(a, b-1, cnt + 1);
-		searching(a, b+1, cnt + 1);
+		searching(a, b / 2, cnt + 1);
+		searching(a, b - 1, cnt + 1);
+		searching(a, b + 1, cnt + 1);
 	}
 	else {
 		searching(a, b - 1, cnt + 1);
 		searching(a, b + 1, cnt + 1);
 	}
 }
+int chai(int a, int b) {
+	int tmp = a - b;
+	if (tmp < 0)
+		tmp = -tmp;
+	return tmp;
+}
 int main(void) {
 	int N, K;
+	int tmp1,tmp2,cnt=0;
 	cin >> N >> K;
-	mini = K - N;
+	tmp1 = N;
+	while (tmp1 < K) {
+		tmp2 = tmp1;
+		tmp1 = tmp1 * 2;
+		if (chai(tmp2, K) < chai(tmp1, K))
+			break;
+		cnt++;
+	}
+	mini = K-tmp2+cnt;
 	if (mini < 0)
 		mini = -mini;
 	searching(N, K, 0);
