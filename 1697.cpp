afeda20f780806848e5459163e39c8bec337ef8f
dngur6344 <43164924+dngur6344@users.noreply.github.com>
2019-02-27 15:38:35 +0900

    1697번

--- 1697.cpp
+++ 1697.cpp
@@ -2,45 +2,43 @@
 #include<queue>
 using namespace std;
 int mini = -1;
-void searching(int a, int b, int cnt) {
-	if (mini < cnt || a<0)
-		return;
-	if (a == b) {
-		mini = cnt;
-		return;
+int visit[100001] = { 0, };
+queue<int> q;
+void searching(int a, int b) {
+	int tmp;
+	int cnt = 0;
+	int num;
+	q.push(a);
+	while (!q.empty()) {
+		int num = q.size();
+		for (int i = 0; i < num; i++) {
+			tmp = q.front();
+			if (tmp == b) {
+				mini = cnt;
+				return;
+			}
+			if (tmp + 1 <= 100000 &&visit[tmp + 1] == 0) {
+				q.push(tmp + 1);
+				visit[tmp + 1] = 1;
+			}
+			if (tmp - 1 >= 0&&visit[tmp - 1] == 0 ) {
+				q.push(tmp - 1);
+				visit[tmp - 1] = 1;
+			}
+			if (tmp * 2 <= 100000&&visit[tmp * 2] == 0) {
+				q.push(tmp * 2);
+				visit[tmp * 2] = 1;
+			}
+			q.pop();
+		}
+		cnt++;
 	}
-	if (b % 2 == 0) {
-		searching(a, b / 2, cnt + 1);
-		searching(a, b - 1, cnt + 1);
-		searching(a, b + 1, cnt + 1);
-	}
-	else {
-		searching(a, b - 1, cnt + 1);
-		searching(a, b + 1, cnt + 1);
-	}
-}
-int chai(int a, int b) {
-	int tmp = a - b;
-	if (tmp < 0)
-		tmp = -tmp;
-	return tmp;
 }
 int main(void) {
 	int N, K;
 	int tmp1,tmp2,cnt=0;
 	cin >> N >> K;
-	tmp1 = N;
-	while (tmp1 < K) {
-		tmp2 = tmp1;
-		tmp1 = tmp1 * 2;
-		if (chai(tmp2, K) < chai(tmp1, K))
-			break;
-		cnt++;
-	}
-	mini = K-tmp2+cnt;
-	if (mini < 0)
-		mini = -mini;
-	searching(N, K, 0);
+	searching(N, K);
 	cout << mini << endl;
 	return 0;
 }
