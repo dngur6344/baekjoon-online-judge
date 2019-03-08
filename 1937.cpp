923778889abe11537d2dff07974691dd10801d15
dngur6344 <43164924+dngur6344@users.noreply.github.com>
2019-03-08 19:19:36 +0900

    1937번-dp로 해봤음 응 그래도 시간초과~

--- 1937.cpp
+++ 1937.cpp
@@ -4,30 +4,10 @@ using namespace std;
 //int **arr;
 int arr[1000][1000];
 int **visit;
-int maxx = 0;
-void searching(int cnt, int i, int j, int n, int maxim) {
-	cnt++;
-	visit[i][j] = 1;
-	if (i - 1 >= 0 && arr[i - 1][j]>arr[i][j])
-		searching(cnt, i - 1, j, n,maxim);
-	if (i + 1<n&&arr[i + 1][j]>arr[i][j] )
-		searching(cnt, i + 1, j, n,maxim);
-	if (j - 1 >= 0 && arr[i][j - 1]>arr[i][j])
-		searching(cnt, i, j - 1, n,maxim);
-	if (j + 1<n&&arr[i][j + 1]>arr[i][j])
-		searching(cnt, i, j + 1, n,maxim);
-	if (cnt > maxx)
-		maxx = cnt;
-	else
-		visit[i][j] = 0;
-	if (maxx <= maxim)
-		visit[i][j] = 0;
-	return;
-}
 void init(int n) {
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			visit[i][j] = 0;
+			visit[i][j] = 1;
 		}
 	}
 }
@@ -46,14 +26,43 @@ int main(void) {
 		}
 	}
 	init(N);
-	for (int i = 0; i<N; i++) {
-		for (int j = 0; j<N; j++) {
-			if (visit[i][j] == 1)continue;
-			searching(0, i, j, N,maxim);
-			if (maxim<maxx) {
-				maxim = maxx;
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			if(i>0&&arr[i-1][j]>arr[i][j]){
+				if(visit[i][j]>=visit[i-1][j]||visit[i-1][j]==1){
+					visit[i-1][j]=visit[i][j]+1;
+					i=i-1;
+					j=j-1;
+					continue;
+				}
 			}
-			maxx = 0;
+			if(i+1<N&&arr[i+1][j]>arr[i][j]){
+				if(visit[i][j]>=visit[i+1][j]||visit[i+1][j]==1){
+					visit[i+1][j]=visit[i][j]+1;
+					i=i+1;
+					j=j-1;
+					continue;
+				}
+			}
+			if(j>0&&arr[i][j-1]>arr[i][j]){
+				if(visit[i][j]>=visit[i][j-1]||visit[i][j-1]==1){
+					visit[i][j-1]=visit[i][j]+1;
+					j=j-2;
+					continue;
+				}
+			}
+			if(j+1<N&&arr[i][j+1]>arr[i][j]){
+				if(visit[i][j]>=visit[i][j+1]||visit[i][j+1]==1){
+					visit[i][j+1]=visit[i][j]+1;
+					continue;
+				}
+			}
+		}
+	}
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			if(visit[i][j]>maxim)
+				maxim=visit[i][j];
 		}
 	}
 	cout << maxim << endl;
