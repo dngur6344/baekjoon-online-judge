95d8d3d5ecd8fb58ec457d017120e20f18d74ebf
dngur6344 <43164924+dngur6344@users.noreply.github.com>
2019-03-09 12:54:11 +0900

    1937번-일단은 미완성...

--- 1937.cpp
+++ 1937.cpp
@@ -1,90 +1,51 @@
 #include<iostream>
 using namespace std;
-//int **arr;
 int arr[1000][1000];
 int visit[1000][1000];
-int maxim=0;
-int max_i=-1;
-int max_j=-1;
-int flag=0;
-void init(int n) {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			visit[i][j] = 1;
-		}
+int searching(int N, int i, int j) {
+	if (visit[i][j] != 0)return visit[i][j];
+	visit[i][j] = 1;
+	if (i>0 && arr[i - 1][j]>arr[i][j]) {
+		//visit[i - 1][j] = visit[i][j] + 1;
+		if(visit[i][j] >= visit[i - 1][j])
+			visit[i][j] = searching(N, i - 1, j) + 1;
 	}
-}
-void searching(int N,int i,int j){
-	if(maxim<visit[i][j]){
-		maxim=visit[i][j];
-		flag=1;
+	if (i + 1<N&&arr[i + 1][j]>arr[i][j]) {
+		//visit[i + 1][j] = visit[i][j] + 1;
+		if (visit[i][j] >= visit[i + 1][j])
+			visit[i][j] = searching(N, i + 1, j) + 1;
 	}
-	if(i>0&&arr[i-1][j]>arr[i][j]){
-		if(visit[i][j]>=visit[i-1][j]||visit[i-1][j]==1){
-			if(i==max_i&&j==max_j){
-				maxim+=visit[i][j];
-			}
-			else{
-				visit[i-1][j]=visit[i][j]+1;
-				searching(N,i-1,j);
-			}
-		}
+	if (j>0 && arr[i][j - 1]>arr[i][j]) {
+		//visit[i][j - 1] = visit[i][j] + 1;
+		if (visit[i][j] >= visit[i][j - 1])
+			visit[i][j] = searching(N, i, j - 1) + 1;
 	}
-	if(i+1<N&&arr[i+1][j]>arr[i][j]){
-		if(visit[i][j]>=visit[i+1][j]||visit[i+1][j]==1){
-			if(i==max_i&&j==max_j){
-				maxim+=visit[i][j];
-			}
-			else{
-				visit[i+1][j]=visit[i][j]+1;
-				searching(N,i+1,j);
-			}
-		}
+	if (j + 1<N&&arr[i][j + 1]>arr[i][j]) {
+		//visit[i][j + 1] = visit[i][j] + 1;
+		if (visit[i][j] >= visit[i][j + 1])
+			visit[i][j] = searching(N, i, j + 1) + 1;
 	}
-	if(j>0&&arr[i][j-1]>arr[i][j]){
-		if(visit[i][j]>=visit[i][j-1]||visit[i][j-1]==1){
-			if(i==max_i&&j==max_j){
-				maxim+=visit[i][j];
-			}
-			else{
-				visit[i][j-1]=visit[i][j]+1;
-				searching(N,i,j-1);
-			}
-		}
-	}
-	if(j+1<N&&arr[i][j+1]>arr[i][j]){
-		if(visit[i][j]>=visit[i][j+1]||visit[i][j+1]==1){
-			if(i==max_i&&j==max_j){
-				maxim+=visit[i][j];
-			}
-			else{
-				visit[i][j+1]=visit[i][j]+1;
-				searching(N,i,j+1);
-			}
-		}
-	}
+	return visit[i][j];
 }
 int main(void) {
 	int N;
 	int tmp;
+	int maxim;
 	cin >> N;
 	for (int i = 0; i<N; i++) {
 		for (int j = 0; j<N; j++) {
 			cin >> arr[i][j];
 		}
 	}
-	init(N);
-	for(int i=0;i<N;i++){
-		for(int j=0;j<N;j++){
-			if(visit[i][j]==1)
-				searching(N,i,j);
-				if(flag==1){
-					max_i=i;
-					max_j=j;
-					flag=0;
-				}
+	maxim = 1;
+	for (int i = 0; i<N; i++) {
+		for (int j = 0; j<N; j++) {
+			tmp=searching(N, i, j);
+			if (tmp > maxim)
+				maxim = tmp;
 		}
 	}
+
 	cout << maxim << endl;
 	return 0;
 }
