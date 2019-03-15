e7bcaaa87df278c7f94275eefa0b59bc27e45a6f
dngur6344 <43164924+dngur6344@users.noreply.github.com>
2019-03-16 00:34:45 +0900

    1068번

--- 1068.cpp
+++ 1068.cpp
@@ -6,19 +6,22 @@ int main(void) {
 	cin >> N;
 	deque<int> dq(N);
 	deque<int> arr;
+	deque<int> visit(N);
 	for (int i = 0; i < N; i++) {
 		cin >> dq[i];
 	}
 	cin >> M;
 	dq[M] = -2;
+	visit[M] = -2;
 	for (int i = 0; i < dq.size(); i++) {
 		if (dq[i] == M) {
 			arr.push_back(i);
 		}
 	}
 	while (arr.empty() != true) {
+		dq[arr.front()] = -2;
+		visit[arr.front()] = -2;
 		for (int i = 0; i < dq.size(); i++) {
-			dq[arr.front()] = -2;
 			if (dq[i] == arr.front()) {
 				arr.push_back(i);
 			}
@@ -29,22 +32,13 @@ int main(void) {
 		if (dq[i] == -2)continue;
 		for (int j = 0; j < dq.size(); j++) {
 			if (i == dq[j]) {
-				arr.push_back(j);
-				dq[i] = -2;
+				visit[i] = -2;
+				break;
 			}
-		}
-		while (arr.empty() != true) {
-			for (int j = 0; j < dq.size(); j++) {
-				if (dq[j] == arr.front()) {
-					arr.push_back(j);
-					dq[arr.front()] = -2;
-				}
-			}
-			arr.pop_front();
 		}
 	}
 	for (int i = 0; i < dq.size(); i++) {
-		if (dq[i] != -2)cnt++;
+		if (visit[i] != -2)cnt++;
 	}
 	cout << cnt << endl;
 	return 0;
