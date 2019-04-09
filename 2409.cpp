5475cfa1676b93928bb2579fdac15cdf4460c904
dngur6344 <43164924+dngur6344@users.noreply.github.com>
2019-04-09 21:07:42 +0900

    2409번-시간초과

--- 2409.cpp
+++ 2409.cpp
@@ -2,24 +2,46 @@
 #include<deque>
 #include<algorithm>
 using namespace std;
+class two{
+private:
+public:
+	int val;
+	int visit;
+};
 deque<int> dq;
-deque<int> dq2;
+deque<two> dq2;
 int max_val=0;
-void finding(deque<int> visit,int cnt,int start){
+int min_search(){
+	int res=dq2[0].val;
+	for(int i=1;i<dq2.size();i++){
+		if(dq2[i].visit==0&&dq2[i].val<res)
+			res=dq2[i].val;
+	}
+	return res;
+}
+int max_search(){
+	int res=dq[0];
+	for(int i=1;i<dq.size();i++){
+		if(dq[i]>res)
+			res=dq[i];
+	}
+	return res;
+}
+void finding(int cnt,int start){
 	int tmp;
-	if(start==dq2.size()){
+	if(min_search()>max_search()){
 		if(cnt>max_val)max_val=cnt;
 		return;
 	}
 	for(int i=start;i<dq2.size();i++){
-		if(visit[i]==0){
-			tmp=dq2[i];
+		if(dq2[i].visit==0){
+			tmp=dq2[i].val;
 			for(int j=0;j<dq.size();j++){
-				if(dq[j]>=dq2[i]){
+				if(dq[j]>=dq2[i].val){
 					dq[j]=dq[j]-tmp;
-					visit[i]=1;
-					finding(visit,cnt+1,i+1);
-					visit[i]=0;
+					dq2[i].visit=1;
+					finding(cnt+1,i+1);
+					dq2[i].visit=0;
 					dq[j]=dq[j]+tmp;
 				}
 			}
@@ -28,6 +50,7 @@ void finding(deque<int> visit,int cnt,int start){
 }
 int main(void){
 	int M,N,tmp;
+	two temper;
 	cin>>M;
 	for(int i=0;i<M;i++){
 		cin>>tmp;
@@ -36,12 +59,13 @@ int main(void){
 	cin>>N;
 	for(int i=0;i<N;i++){
 		cin>>tmp;
-		dq2.push_back(tmp);
+		temper.val=tmp;
+		temper.visit=0;
+		dq2.push_back(temper);
 	}
-	deque<int> visit(N,0);
 	//sort(dq.begin(),dq.end(),greater<int>());
 	//sort(dq2.begin(),dq2.end());
-	finding(visit,0,0);
+	finding(0,0);
 	cout<<max_val<<endl;
 	return 0;
 }
